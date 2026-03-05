3da5135f8b97efdad01b8d95d11624e687c21f1b
aliabbas-2012 <itsgeniusstar@gmail.com>
2026-03-05 17:32:57 +0500

    idea

--- app/scraper.py
+++ app/scraper.py
@@ -6,6 +6,7 @@ def fetch_mcb_data(url, start_date, end_date, cat):
     try:
         scraper = cloudscraper.create_scraper(
             browser={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
                 'browser': 'chrome',
                 'platform': 'windows',
                 'mobile': False
