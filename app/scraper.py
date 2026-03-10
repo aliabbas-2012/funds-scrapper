26a4a1e241407348da59b83b6c9c0325db560add
aliabbas-2012 <itsgeniusstar@gmail.com>
2026-03-10 23:52:39 +0500

    scrapper

--- app/scraper.py
+++ app/scraper.py
@@ -1,54 +1,49 @@
 import cloudscraper
 from bs4 import BeautifulSoup
 
-
 def fetch_mcb_data(url, start_date, end_date, cat):
-    try:
-        scraper = cloudscraper.create_scraper(
-            browser={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
-                'browser': 'chrome',
-                'platform': 'windows',
-                'mobile': False
-            }
-        )
 
-        homepage = "https://www.mcbfunds.com/"
-        scraper.get(homepage)
-
-        payload = {
-            "a[from]": start_date,
-            "a[to]": end_date,
-            "a[cat]": cat,
-            "btn_submit": "Submit"
+    scraper = cloudscraper.create_scraper(
+        browser={
+            "browser": "chrome",
+            "platform": "windows",
+            "mobile": False
         }
+    )
 
-        headers = {
-            "User-Agent": scraper.headers["User-Agent"],
-            "Referer": url,
-            "Origin": "https://www.mcbfunds.com",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
+    # open page where form exists (IMPORTANT)
+    scraper.get(url)
 
-        response = scraper.post(url, data=payload, headers=headers)
+    payload = {
+        "a[from]": start_date,
+        "a[to]": end_date,
+        "a[cat]": cat,
+        "btn_submit": "Submit"
+    }
 
-        if response.status_code != 200:
-            return {"error": f"Blocked: {response.status_code}"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "Referer": url,
+        "Origin": "https://www.mcbfunds.com",
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
 
-        soup = BeautifulSoup(response.text, "lxml")
-        table = soup.find("table", {"id": "table2"})
+    response = scraper.post(url, data=payload, headers=headers)
 
-        data = []
+    print(response.status_code)
 
-        if table:
-            headers = [th.text.strip() for th in table.find_all("th")]
+    soup = BeautifulSoup(response.text, "lxml")
+    table = soup.find("table", {"id": "table2"})
 
-            for row in table.find_all("tr")[1:]:
-                cols = [td.text.strip() for td in row.find_all("td")]
-                if cols:
-                    data.append(dict(zip(headers, cols)))
+    data = []
 
-        return data
+    if table:
+        headers = [th.text.strip() for th in table.find_all("th")]
 
-    except Exception as e:
-        return {"error": str(e)}
+        for row in table.find_all("tr")[1:]:
+            cols = [td.text.strip() for td in row.find_all("td")]
+            if cols:
+                data.append(dict(zip(headers, cols)))
+
+    return data
